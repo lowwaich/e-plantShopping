ead43a325b0145b048c2e4f8e5338f60794cc413
lowwaich <lowwaich@hotmail.com>
2026-09-09 06:22:11 -0400

    updated CartItem.jsx CartSlice.jsx ProductList.jsx

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -8,6 +8,7 @@ export const CartSlice = createSlice({
     reducers: {
 
         addItem: (state, action) => {
+            console.log("Payload arriving in Redux:", action.payload);
             const { name, image, cost } = action.payload; // Destructure product details from the action payload
             // Check if the item already exists in the cart by comparing names
             const existingItem = state.items.find(item => item.name === name);
--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import './ProductList.css'
 import CartItem from './CartItem';
-import addItem from './CartSlice';
+import { addItem } from './CartSlice';
 
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
@@ -256,6 +257,8 @@ function ProductList({ onHomeClick }) {
     };
 
     const [addedToCart, setAddedToCart] = useState({});
+
+    const dispatch = useDispatch();
 
     const handleAddToCart = (product) => {
         dispatch(addItem(product)); // Dispatch the action to add the product to the cart (Redux action)
